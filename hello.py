2354f0ef538e1436dd0fae15965a74394428a2a0
DogMey <kevinsantiagoram25@gmail.com>
2025-07-19 11:19:51 -0500

    PrimeraParte

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Hola mundo
+
+print("¡Hola, mundo!")
+
+"""
+Comentario sin limite de línea
+Este es un comentario que se extiende
+"""
+
+'''
+Este igual es un comentario
+que se extiende por varias líneas
+'''
+
+print (3 != 4)  # Esto imprime True porque 3 no es igual a 4
