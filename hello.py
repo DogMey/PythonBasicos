59d236688a28a92e601b5723a4403774f822d43a
DogMey <kevinsantiagoram25@gmail.com>
2025-07-19 11:23:32 -0500

    TiposDatos

--- hello.py
+++ hello.py
@@ -13,3 +13,9 @@ que se extiende por varias líneas
 '''
 
 print (3 != 4)  # Esto imprime True porque 3 no es igual a 4
+
+# Ejemplo de tipos de variables
+numero = 10  # Variable de tipo entero
+texto = "Hola"  # Variable de tipo cadena de texto
+decimal = 3.14  # Variable de tipo flotante
+booleano = True  # Variable de tipo booleano
